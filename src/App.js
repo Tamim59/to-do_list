ccec34d8890383fd454df7cd8d1cd29e384eac2b
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2023-07-14 22:52:13 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  let [taskarr,setTaskarr] = useState([])
+  let [text,setText] = useState("")
+  let [error,setError] = useState(false)
+  let [btn,setbtn] = useState(true)
+  let [editid,setEditId] = useState("")
+
+  let handelChange = (e)=>{
+    setText(e.target.value)
+  }
+
+  let handelClick = ()=>{
+    if(text){
+      if(error){
+      setError(false)
+      }
+      taskarr.push(text)
+      let arr = [...taskarr]
+      setTaskarr(arr)
+      setText("")
+    }else{
+      if(!error){
+      setError(true)
+      }
+    }
+
+  }
+
+  let handledelete = (id)=>{
+    taskarr.splice(id,1);
+    let arr = [...taskarr]
+    setTaskarr(arr)
+  }
+
+  let handelEdit = (edit,id)=>{
+    setbtn(false)
+    setText(edit);
+    setEditId(id)
+  }
+  let handelCancle =()=>{
+    setbtn(true)
+    setText("")
+  }
+
+  let handelUpdate = ()=>{
+    taskarr[editid] = text
+    let arr = [...taskarr]
+    setTaskarr(arr)
+    setText("")
+    setbtn(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input onChange={handelChange} value={text}/>
+      {
+        btn 
+      ? 
+        <button onClick={handelClick}>Submit</button>
+      :
+        <>
+        <button onClick={handelUpdate} value={text}>Update</button>
+        <button onClick={()=>handelCancle()} value={text}>Cancle</button>
+        </>
+      }
+      
+      
+      {error && <h3>Enter a value</h3>}
+      <ol>
+      {taskarr.map((item,index)=>(
+          <li key={index}>{item}<button onClick={()=>handledelete(index)}>Delete</button><button onClick={()=>handelEdit(item,index)}>Edit</button></li>
+        ))}
+      </ol>
+    </>
   );
 }
 
